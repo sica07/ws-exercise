ce14392261e6fb8265b9b11c4f44cfda74b0b26d
Marius Matei <mariusmatei31@gmail.com>
2017-11-03 21:08:23 +0200

    add PSR-4 file structure

---
+++ srv/src/BinaryEcho.php
@@ -0,0 +1,71 @@
+<?php
+namespace MyApp;
+
+use Ratchet\ConnectionInterface;
+
+
+class BinaryEcho implements \Ratchet\WebSocket\MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, \Ratchet\RFC6455\Messaging\MessageInterface $msg)
+    {
+        if (!$msg->isBinary()) {
+            echo 'The data received is not an image!';
+            return;
+        }
+
+        if ($filename = $this->uploadImage($msg)) {
+            $from->send('/srv/storage/' . $filename);
+        }
+
+
+        $this->notifyClients($msg, $from);
+    }
+
+    private function uploadImage($img)
+    {
+        $filename = rand() . '.jpg';
+        try {
+            $result = file_put_contents('srv/storage/' . $filename, $img);
+            //message to sender
+            echo 'The file ' . $filename . ' was uploaded successfully';
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return;
+        }
+
+        return $filename;
+    }
+
+    private function notifyClients($img, $from)
+    {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($img);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        echo "Conection close";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+    }
+}
